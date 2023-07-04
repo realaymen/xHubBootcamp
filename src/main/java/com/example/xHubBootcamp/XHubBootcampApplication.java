10faeb01ffd589fa787544ead2a49cba2e186f3c
Aymen <bysfaymen@gmail.com>
2023-07-04 10:27:44 +0100

    first-commit

---
+++ src/main/java/com/example/xHubBootcamp/XHubBootcampApplication.java
@@ -0,0 +1,13 @@
+package com.example.xHubBootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XHubBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XHubBootcampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/xHubBootcamp/XHubBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xHubBootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XHubBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
